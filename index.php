c8d955d4228c25d678496cecfbb36d010bd40523
Joao Victor <jvictorsousa2015@gmail.com>
2025-11-06 17:51:57 -0300

    autorizaçãop de CORS e metodos options na api

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers:Authorization, Content-Type, x-xsrf-token, x-csrtoken, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
